86969ef377c705126da66816d0de6b8578481c12
DanBurbach <dburbach1982@gmail.com>
2020-03-04 13:10:54 -0800

    framework for project, include routing, 404 re-direct, and proper folder organization

---
+++ src/components/App.jsx
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
+import * as ROUTES from "../constants/routes";
+
+import Main from './Main';
+import Error404 from "./Error404";
+
+import './../assets/App.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <BrowserRouter>
+          <Switch>
+              <Route exact path={ROUTES.MAIN} component={Main} />
+              <Route component={Error404} />
+          </Switch>
+        </BrowserRouter>
+      </div>
+    );
+  }
+}
+
+export default App;
